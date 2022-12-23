07e53302a70dbefb9da46b5252d4dca8159090eb
Monsuru Adepeju <m.adepeju@mmu.ac.uk>
2022-12-23 21:42:09 +0000

    update

--- scripts/render-poster.R
+++ scripts/render-poster.R
@@ -1,8 +1,8 @@
 # Set parameters
 output_file <- "flexdashboard-poster.html"
-height <- 1200
+width <- 1200
 #poster_width <- 23.4 # inches
-poster_height <- 23.4
+poster_width <- 23.4
 res <- 300
 
 # Render document
@@ -15,10 +15,10 @@ rmarkdown::render(
 webshot::webshot(
   url = paste0("output/", output_file),
   file = "output/poster.png",
-  vwidth = floor(height * sqrt(2)),
-  vheight = height,          # Use A series aspect ratio
+  vwidth = width,
+  vheight = floor(width * sqrt(2)),          # Use A series aspect ratio
   delay = 1,                                 # Wait to allow all element to load
-  zoom = poster_height / (height / res)        # Adjust elements relative size
+  zoom = poster_width / (width / res)        # Adjust elements relative size
 )
 
 # End of script
